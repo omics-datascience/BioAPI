62ee823e7882ab66b516445af937dfa9b883a606
Genaro Camele <genarocamele@hotmail.com>
2022-05-31 17:46:01 -0300

    Added development and production config in Flask app + Refactoring

--- bio-api/bioapi.py
+++ bio-api/bioapi.py
@@ -1,231 +1,270 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+from typing import Optional
 
 from flask import Flask
 from flask import jsonify
 from flask import make_response
 from flask import abort
-from flask import url_for
 from flask import render_template
 from flask import request
-from flask import redirect
 from flask_cors import CORS
 import urllib.parse
-import os
 import pymongo
-import json
 import re
 import os
-import requests
 import configparser
-import logging 
+import logging
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
-#Levanto configuracion
+# Gets production flag
+IS_DEBUG: bool = os.environ.get('DEBUG', 'true') == 'true'
+
+# Levanto configuracion
 Config = configparser.ConfigParser()
 Config.read("config.txt")
 
-#configuracion log
+# configuracion log
 logging.getLogger("urllib3").setLevel(logging.DEBUG)
-logging.basicConfig(filename=Config.get('log', 'file'), filemode='a', format='%(asctime)s\t%(name)s\t%(levelname)s\t%(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.DEBUG)
+# In development logs in console, in production logs in file
+if not IS_DEBUG:
+    logging.basicConfig(filename=Config.get('log', 'file'), filemode='a',
+                        format='%(asctime)s\t%(name)s\t%(levelname)s\t%(message)s', datefmt='%d-%b-%y %H:%M:%S',
+                        level=logging.DEBUG)
 logging.info('BIO-API iniciado')
 
-#Defino header de respuestas (puede que este al pedo)
-headers = {}
-headers["Content-Type"] = "application/json"
+# Defino header de respuestas (puede que este al pedo)
+headers = {"Content-Type": "application/json"}
 
-#Funcion de conexion a MongoDB
-def conexion_mongodb(url, port, user=None, password=None):
-	#Para cuando agreguemos user y pass
-	#myclient = pymongo.MongoClient("mongodb://"+user+":"+password+"@"+url+":"+port+"/?authSource=genomics_dbs&authMechanism=SCRAM-SHA-1")
-	myclient = pymongo.MongoClient("mongodb://"+url+":"+port+"/?authSource=genomics_dbs&authMechanism=SCRAM-SHA-1")
-	return myclient["genomics_dbs"]
 
-#Conexion a MongoDB
+# Funcion de conexion a MongoDB
+def conexion_mongodb(url: str, port: str, user: str = '', password: str = '', db: str = 'bio_api'):
+    myclient = pymongo.MongoClient(f"mongodb://{user}:{password}@{url}:{port}/?authSource=admin")
+    return myclient[db]
+
+
+# Conexion a MongoDB
 try:
-	mydb = conexion_mongodb(Config.get('mongodb', 'host'),Config.get('mongodb', 'port'))
+    if IS_DEBUG:
+        host = Config.get('mongodb', 'host')
+        port = Config.get('mongodb', 'port')
+        user = Config.get('mongodb', 'user')
+        password = Config.get('mongodb', 'pass')
+        db = Config.get('mongodb', 'db_name')
+    else:
+        host = os.environ.get('MONGO_HOST')
+        port = os.environ.get('MONGO_PORT')
+        user = os.environ.get('MONGO_USER')
+        password = os.environ.get('MONGO_PASSWORD')
+        db = os.environ.get('MONGO_DB')
+
+        if not host or not port or not db:
+            logging.error(f'Host ({host}), port ({port}) or db ({db}) is invalid', exc_info=True)
+            exit(-1)
+
+    #     MONGO_INITDB_ROOT_USERNAME
+    # MONGO_INITDB_ROOT_PASSWORD
+    # MONGO_INITDB_DATABASE
+    mydb = conexion_mongodb(host, port, user, password, db)
 except Exception as e:
-	logging.error("BioAPI no se pudo conectar a la base de datos MongoDB configurada." + str(e), exc_info=True)
+    logging.error("BioAPI no se pudo conectar a la base de datos MongoDB configurada." + str(e), exc_info=True)
+    exit(-1)
 
 
 #############   Definicion de funciones   #############
 def mapear_gen(gen):
-	# gene = gen.upper()
-	er = re.compile("^"+re.escape(gen)+"$", re.IGNORECASE) 	
-	mycol_hgnc = mydb["hgnc"]		#coneccion a coleccion hgnc
-	dbs = ["hgnc_id", "symbol", "status", "alias_symbol", "prev_symbol", "entrez_id", "ensembl_gene_id", "vega_id", "ucsc_id", "ena", "refseq_accession", "ccds_id", "uniprot_ids", "cosmic", "omim_id", "mirbase", "homeodb", "snornabase", "bioparadigms_slc", "orphanet", "pseudogene", "horde_id", "merops", "imgt", "iuphar", "kznf_gene_catalog", "mamit-trnadb", "cd", "lncrnadb", "enzyme_id", "intermediate_filament_db", "agr"]
-	#armo query
-	or_search = []
-	for db in dbs:
-		q = { db: {"$regex": er} }
-		#q = { db : gene }
-		or_search.append(q)
-	query = { '$or': or_search, "status":"Approved"}
-	proyection = { '_id': 0, 'symbol':1 }
-	#hago consulta a la db
-	mydocs = mycol_hgnc.find(query, proyection)
-	results = []
-	for doc in mydocs:
-		results.append(doc["symbol"])
-	return results
+    # gene = gen.upper()
+    er = re.compile("^" + re.escape(gen) + "$", re.IGNORECASE)
+    mycol_hgnc = mydb["hgnc"]  # coneccion a coleccion hgnc
+    dbs = ["hgnc_id", "symbol", "status", "alias_symbol", "prev_symbol", "entrez_id", "ensembl_gene_id", "vega_id",
+           "ucsc_id", "ena", "refseq_accession", "ccds_id", "uniprot_ids", "cosmic", "omim_id", "mirbase", "homeodb",
+           "snornabase", "bioparadigms_slc", "orphanet", "pseudogene", "horde_id", "merops", "imgt", "iuphar",
+           "kznf_gene_catalog", "mamit-trnadb", "cd", "lncrnadb", "enzyme_id", "intermediate_filament_db", "agr"]
+    # armo query
+    or_search = []
+    for db in dbs:
+        q = {db: {"$regex": er}}
+        # q = { db : gene }
+        or_search.append(q)
+    query = {'$or': or_search, "status": "Approved"}
+    proyection = {'_id': 0, 'symbol': 1}
+    # hago consulta a la db
+    mydocs = mycol_hgnc.find(query, proyection)
+    results = []
+    for doc in mydocs:
+        results.append(doc["symbol"])
+    return results
 
-def buscar_grupo_gen(gen): 	#AGREGAR LO QUE PASA SI NO PERTENECE A NINGUN gene_group_id (EJ gen:AADACP1)
-	results = { 'locus_group':None , 'locus_type':None, 'gene_group':None, 'gene_group_id':None }
-	mycol_hgnc = mydb["hgnc"]		#coneccion a coleccion hgnc
-	query = { "symbol": gen, "status":"Approved"}
-	#hago consulta a la db
-	mydocs = mycol_hgnc.find(query)
-	if mydocs.count() == 1:
-		results['locus_group'] = mydocs[0]['locus_group']
-		results['locus_type'] = mydocs[0]['locus_type']
-		if "gene_group" in list(mydocs[0].keys()):
-			if type(mydocs[0]['gene_group']) == list:
-				results['gene_group'] = mydocs[0]['gene_group']
-				results['gene_group_id'] = mydocs[0]['gene_group_id']
-			else:
-				results['gene_group'] = [mydocs[0]['gene_group']]
-				results['gene_group_id'] = [mydocs[0]['gene_group_id']]
-		 
-	return results
 
-def buscar_genes_mismo_grupo(id_grupo): 	
-	mycol_hgnc = mydb["hgnc"]		#coneccion a coleccion hgnc
-	query = { 'gene_group_id': id_grupo}
-	proyection = { '_id': 0, 'symbol':1 }
-	#hago consulta a la db
-	mydocs = mycol_hgnc.find(query, proyection)
-	results = []
-	for doc in mydocs:
-		results.append(doc["symbol"])
-	return results
+def buscar_grupo_gen(gen):  # AGREGAR LO QUE PASA SI NO PERTENECE A NINGUN gene_group_id (EJ gen:AADACP1)
+    results = {'locus_group': None, 'locus_type': None, 'gene_group': None, 'gene_group_id': None}
+    mycol_hgnc = mydb["hgnc"]  # coneccion a coleccion hgnc
+    query = {"symbol": gen, "status": "Approved"}
+    # hago consulta a la db
+    mydocs = mycol_hgnc.find(query)
+    if mydocs.count() == 1:
+        results['locus_group'] = mydocs[0]['locus_group']
+        results['locus_type'] = mydocs[0]['locus_type']
+        if "gene_group" in list(mydocs[0].keys()):
+            if type(mydocs[0]['gene_group']) == list:
+                results['gene_group'] = mydocs[0]['gene_group']
+                results['gene_group_id'] = mydocs[0]['gene_group_id']
+            else:
+                results['gene_group'] = [mydocs[0]['gene_group']]
+                results['gene_group_id'] = [mydocs[0]['gene_group_id']]
+
+    return results
+
+
+def buscar_genes_mismo_grupo(id_grupo):
+    mycol_hgnc = mydb["hgnc"]  # coneccion a coleccion hgnc
+    query = {'gene_group_id': id_grupo}
+    proyection = {'_id': 0, 'symbol': 1}
+    # hago consulta a la db
+    mydocs = mycol_hgnc.find(query, proyection)
+    results = []
+    for doc in mydocs:
+        results.append(doc["symbol"])
+    return results
+
 
 #############   Definicion de endpoints #############
-@app.route("/")				#Pantalla de inicio
+@app.route("/")  # Pantalla de inicio
 def inicio():
-	# return redirect(url_documentacion, code=302, Response=None)
-	return render_template('homePage.html')
+    # return redirect(url_documentacion, code=302, Response=None)
+    return render_template('homePage.html')
 
-@app.route("/ping")			#Para chequear que este levantada la bioapi
+
+@app.route("/ping")  # Para chequear que este levantada la bioapi
 def ping_ok():
     output = "ok"
-    return make_response(output,200,headers)
+    return make_response(output, 200, headers)
 
-@app.route("/bioapi-map")	#Lista todos los endpoint de bioapi
+
+@app.route("/bioapi-map")  # Lista todos los endpoint de bioapi
 def list_routes():
-    output = { "endpoints" : []}
+    output = {"endpoints": []}
     for rule in app.url_map.iter_rules():
         methods = ','.join(rule.methods)
         line = urllib.parse.unquote("{:50s} {:20s} {}".format(rule.endpoint, methods, rule))
         output["endpoints"].append(line)
-    return make_response(output,200,headers)
+    return make_response(output, 200, headers)
 
-@app.route("/gene-symbol")			
-#recibe un ID del gen en cualquier estandard y devuelve el ID del Gen estandarizado. En caso de que no se encuentre debe retornar null en el valor.
+
+@app.route("/gene-symbol")
+# recibe un ID del gen en cualquier estandard y devuelve el ID del Gen estandarizado. En caso de que no se encuentre debe retornar null en el valor.
 def genSymbol():
-	respuesta = { "gene" : None}
-	try:
-		#Controlo parametros
-		args = request.args
-		if "gene_id" not in args:
-			abort(400, "gene_id is mandatory.")	
-		gv = mapear_gen(args.get("gene_id"))
-		if len(gv)>0:
-			respuesta["gene"] = gv
-	except TypeError as e:
-		abort(400,e)
-	except ValueError as e:
-		abort(400,e)
-	except KeyError as e:
-		abort(400,e)
-	return make_response(respuesta,200,headers)
+    respuesta = {"gene": None}
+    try:
+        # Controlo parametros
+        args = request.args
+        if "gene_id" not in args:
+            abort(400, "gene_id is mandatory.")
+        gv = mapear_gen(args.get("gene_id"))
+        if len(gv) > 0:
+            respuesta["gene"] = gv
+    except TypeError as e:
+        abort(400, e)
+    except ValueError as e:
+        abort(400, e)
+    except KeyError as e:
+        abort(400, e)
+    return make_response(respuesta, 200, headers)
 
-@app.route("/genes-symbols")			
+
+@app.route("/genes-symbols")
 def genSymbols():
-	respuesta = { "genes" : []}
-	try:
-		#Controlo parametros
-		args = request.args
-		if "genes_ids" not in args:
-			abort(400, "genes_ids is mandatory.")
-		
-		genes = args.get("genes_ids").split(",")
-		for gene in genes:
-			gv = mapear_gen(gene)
-			if len(gv)>0:
-				respuesta["genes"].append(gv)
-			else:
-				respuesta["genes"].append(None)
-	except TypeError as e:
-		abort(400,e)
-	except ValueError as e:
-		abort(400,e)
-	except KeyError as e:
-		abort(400,e)
-	return make_response(respuesta,200,headers)
+    respuesta = {"genes": []}
+    try:
+        # Controlo parametros
+        args = request.args
+        if "genes_ids" not in args:
+            abort(400, "genes_ids is mandatory.")
 
-@app.route("/genes-same-family")			
+        genes = args.get("genes_ids").split(",")
+        for gene in genes:
+            gv = mapear_gen(gene)
+            if len(gv) > 0:
+                respuesta["genes"].append(gv)
+            else:
+                respuesta["genes"].append(None)
+    except TypeError as e:
+        abort(400, e)
+    except ValueError as e:
+        abort(400, e)
+    except KeyError as e:
+        abort(400, e)
+    return make_response(respuesta, 200, headers)
+
+
+@app.route("/genes-same-family")
 def genes_of_the_same_family():
-	respuesta = { "gene_id" : None, "groups": [], "locus_group": None, "locus_type": None}
-	try:
-		#Controlo parametros
-		args = request.args
-		if "gene_id" not in args:
-			abort(400, "gene_id is mandatory.")
-		approved_symbol = None
-		mapped_gene = mapear_gen(args.get("gene_id"))
-		if len(mapped_gene) == 0:
-			abort(400,"invalid gene identifier")
-		elif len(mapped_gene) >= 2:
-			abort(400,"ambiguous gene identifier. The identifier may refer to more than one HGNC-approved gene ("+",".join(mapped_gene)+")")
-		else:
-			approved_symbol = mapped_gene[0]
-			respuesta["gene_id"] = approved_symbol
-		gene_group = buscar_grupo_gen(approved_symbol)
-		respuesta['locus_group'] = gene_group['locus_group']
-		respuesta['locus_type'] = gene_group['locus_type']
-		if gene_group['gene_group_id'] != None:
-			respuesta["groups"] = []
-			for i in range(0, len(gene_group['gene_group_id'])):
-				g = {}
-				g["gene_group_id"] = gene_group['gene_group_id'][i]
-				g["gene_group"] = gene_group['gene_group'][i]
-				g["genes"] = buscar_genes_mismo_grupo(gene_group['gene_group_id'][i])
-				respuesta["groups"].append(g)
-			
-	
-	except TypeError as e:
-		abort(400,e)
-	except ValueError as e:
-		abort(400,e)
-	except KeyError as e:
-		abort(400,e)
-	return make_response(respuesta,200,headers)
+    respuesta = {"gene_id": None, "groups": [], "locus_group": None, "locus_type": None}
+    try:
+        # Controlo parametros
+        args = request.args
+        if "gene_id" not in args:
+            abort(400, "gene_id is mandatory.")
+        approved_symbol = None
+        mapped_gene = mapear_gen(args.get("gene_id"))
+        if len(mapped_gene) == 0:
+            abort(400, "invalid gene identifier")
+        elif len(mapped_gene) >= 2:
+            abort(400,
+                  "ambiguous gene identifier. The identifier may refer to more than one HGNC-approved gene (" + ",".join(
+                      mapped_gene) + ")")
+        else:
+            approved_symbol = mapped_gene[0]
+            respuesta["gene_id"] = approved_symbol
+        gene_group = buscar_grupo_gen(approved_symbol)
+        respuesta['locus_group'] = gene_group['locus_group']
+        respuesta['locus_type'] = gene_group['locus_type']
+        if gene_group['gene_group_id'] != None:
+            respuesta["groups"] = []
+            for i in range(0, len(gene_group['gene_group_id'])):
+                g = {}
+                g["gene_group_id"] = gene_group['gene_group_id'][i]
+                g["gene_group"] = gene_group['gene_group'][i]
+                g["genes"] = buscar_genes_mismo_grupo(gene_group['gene_group_id'][i])
+                respuesta["groups"].append(g)
 
 
+    except TypeError as e:
+        abort(400, e)
+    except ValueError as e:
+        abort(400, e)
+    except KeyError as e:
+        abort(400, e)
+    return make_response(respuesta, 200, headers)
 
-#Manejo de errores
+
+# Manejo de errores
 @app.errorhandler(400)
 def resource_not_found(e):
-	return jsonify(error=str(e)), 400
+    return jsonify(error=str(e)), 400
+
 
 @app.errorhandler(405)
 def resource_not_found(e):
-	return jsonify(error=str(e)), 405
-	
+    return jsonify(error=str(e)), 405
+
+
 @app.errorhandler(404)
 def resource_not_found(e):
-	return jsonify(error=str(e)), 404
-	
+    return jsonify(error=str(e)), 404
+
+
 @app.errorhandler(409)
 def resource_not_found(e):
-	return jsonify(error=str(e)), 409
-	
+    return jsonify(error=str(e)), 409
+
+
 @app.errorhandler(500)
 def resource_not_found(e):
-	return jsonify(error=str(e)), 500
+    return jsonify(error=str(e)), 500
+
 
 if __name__ == "__main__":
-	port = int(os.environ.get('PORT', Config.get('flask', 'port')))
-	app.run(host='localhost', port=port, debug=True)
+    port_str = os.environ.get('PORT', Config.get('flask', 'port'))
+    port = int(port_str)
+    app.run(host='localhost', port=port, debug=IS_DEBUG)
