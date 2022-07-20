d0000614a2cbcbbafbc53cf9a0ce5b39a175795f
Mauricio Brunner <mauribrunner@gmail.com>
2022-07-20 19:01:54 -0300

    process GTEx database and import it into MongoDB

--- databases/gtex/process_gtex_database.py
+++ databases/gtex/process_gtex_database.py
@@ -1,22 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import csv
 from tqdm import tqdm
 import argparse
+import requests
+from pymongo import MongoClient
+
 
 class C:
     pass
 
 c = C()
 parser = argparse.ArgumentParser(
-    description='Procesa la base de datos GTEx. Genera archivos en Formato Json importables en MongoDB.')
-parser.add_argument('--input_db', help='archivo gct (Gene Cluster Text) correspondiente a la base de datos GTEx v8',
+    description='Procesa la base de datos GTEx y la importa a MongoDB.')
+parser.add_argument('--input_db', help='archivo gct (Gene Cluster Text) correspondiente a la base de datos GTEx',
                     required=True)
-parser.add_argument('--input_annotations', help='archivo txt con los atributos anotados para cada muestra de la base de datos GTEx v8',
+parser.add_argument('--input_annotations', help='archivo txt con los atributos anotados para cada muestra',
                     required=True)
-parser.add_argument('--output_dir', help='Directorio donde se guardaran los archivos CSV generados', required=False, default=None)
+parser.add_argument('--mongodb_ip', help='IP de la base de datos MongoDB', default="localhost", required=False)                    
+parser.add_argument('--mongodb_port', help='Puerto de la base de datos MongoDB', default="27017", required=False)                    
+
 args = parser.parse_args(namespace=c)
+
+def writemongodb(data, tissue, mongo_client):
+    #Conexión a la coleccion
+    gtex_db_tissue = mongo_client[tissue]
+    gtex_db_tissue.insert_many(data)
 
 def parse_tissue_detail(full_tissue_detail: str) -> str:
     aux = full_tissue_detail.split(" - ")
@@ -32,51 +41,48 @@ def get_atributes_of_samples(file_path: str) -> object:
         respuesta[row[0]] = {"tissue": row[5], "tissue_detail": parse_tissue_detail(row[6])}
     return respuesta
 
-def write_data_to_file_csv(file_name, list_of_list_of_elem):
-    with open(file_name, 'a+', newline='') as write_obj:
-        csv_writer = csv.writer(write_obj)
-        for list_of_elem in list_of_list_of_elem:
-            csv_writer.writerow(list_of_elem)
-
 
 if __name__ == '__main__':
     archivo = c.input_db
     anotaciones = c.input_annotations
-    prefijo_archivo_salida = "gtex_v8"
+    database_mongo_ip=c.mongodb_ip
+    database_mongo_port=c.mongodb_port    
     
-    if c.output_dir != None:
-        path_out = str(c.output_dir)
-        if not path_out.endswith("/"):
-            path_out = path_out + "/"
-    else:
-        path_out = ""
+    #Conexión a MongoDB 
+    mongoClient = MongoClient(database_mongo_ip + ":" + database_mongo_port)
+    #Conexión a la base de datos GTEx
+    db = mongoClient["gtex"]
 
+    #Obtengo atributos de la muestra
     ann = get_atributes_of_samples(anotaciones)
 
-    count = 1
+    count=0
     gct_file = open(archivo)
     contenido_db = csv.reader(gct_file, dialect='excel', delimiter='\t')
     gct_file_version = next(contenido_db)   #Guarda version del archivo
     row_col = next(contenido_db)            #Guarda numero de filas y columnas en el archivo
     header = next(contenido_db)             #Guarda valores del header
+    
     docs = {}
     for row in tqdm(contenido_db):          #Por cada linea en la DB (por cada gen)...
         for i in range(2, len(row)):        #Por cada columna en la DB (por cada muestra)
             try:
-                d = [ float(row[i]), row[1], header[i], ann[header[i]]["tissue_detail"] ]
+                d = {"expression": float(row[i]), "gene": row[1], "sample_id": header[i], "tissue_detail": ann[header[i]]["tissue_detail"]}
                 tissue = ann[header[i]]["tissue"]
                 if tissue not in list(docs.keys()):
                     docs[tissue] = []
                 docs[tissue].append(d)
+                count=count+1
                 if len(docs[tissue]) == 1000000:
-                    full_filename = path_out + prefijo_archivo_salida + "_" +tissue + ".csv"
-                    write_data_to_file_csv(full_filename, docs[tissue])
+                    documents = docs[tissue]
                     docs[tissue] = []
+                    writemongodb(documents, tissue, db)
             except KeyError as e:
                 print("Error al obtener los datos de GTEx!. Detalle: " + str(e))
     
     for tissue in list(docs.keys()):
-        full_filename = path_out + prefijo_archivo_salida + "_" +tissue + ".csv"
-        write_data_to_file_csv(full_filename, docs[tissue])
+        writemongodb(docs[tissue] ,tissue, db)
     
     gct_file.close()
+    print("FINALIZADO!\n\tSe cargaron " + str(count) + " registros!")
+    # Son aproximadamente 1.000.000.000 de documentos!
