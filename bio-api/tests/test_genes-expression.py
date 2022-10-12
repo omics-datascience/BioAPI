9f0617db74fde0776840f6d027413803235cc5b6
mauricio <mauribrunner@gmail.com>
2022-10-12 17:45:10 -0300

    documentacion y tests

--- bio-api/tests/test_genes-expression.py
+++ bio-api/tests/test_genes-expression.py
@@ -7,7 +7,7 @@ headers = {
 }
 
 def test_valid_response_format(client):
-    """Tests valid response_format"""
+    """Tests valid response format"""
     gene="FAM87B"
     tissue="Breast"
     data = {    
@@ -20,5 +20,48 @@ def test_valid_response_format(client):
     assert type(res) == list
     assert type(res[0]) == list
     assert type(res[0][0]) == dict
-    claves = list(res[0][0])
+    claves = res[0][0]
     assert "expression" in claves and "gene" in claves
+
+def test_invalid_body_format(client):
+    """Tests invalid body format"""
+    # tejido instead of tissue
+    data = {    
+        "tejido": "Blood",
+        "genes_ids": ["BRCA1", "BRCA2"]
+    }
+    response = client.post(f'{URL_BASE}', data=json.dumps(data), headers=headers)
+    res = json.loads(response.data)
+    assert response.status_code == 400
+    assert res["error"] == "400 Bad Request: tissue is mandatory"
+    # gene instead of genes_ids
+    data = {    
+        "tissue": "Blood",
+        "gene": ["BRCA1"]
+    }
+    response = client.post(f'{URL_BASE}', data=json.dumps(data), headers=headers)
+    res = json.loads(response.data)
+    assert response.status_code == 400
+    assert res["error"] == "400 Bad Request: genes_ids is mandatory"
+    # Str instead of list type in genes_ids
+    data = {    
+        "tissue": "Blood",
+        "genes_ids": "BRCA1"
+    }
+    response = client.post(f'{URL_BASE}', data=json.dumps(data), headers=headers)
+    res = json.loads(response.data)
+    assert response.status_code == 400
+    assert res["error"] == "400 Bad Request: genes_ids must be a list"
+
+def test_invalid_tissues(client):
+    """Tests invalid tissues"""
+    gene="BRCA1"
+    tissue="THISISANINVALIDTISSUE"
+    data = {    
+        "tissue": tissue,
+        "genes_ids": [gene]
+    }
+    response = client.post(f'{URL_BASE}', data=json.dumps(data), headers=headers)
+    res = json.loads(response.data)
+    assert response.status_code == 200
+    assert res == []
