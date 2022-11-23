a9462a9b291c2a4838718d7b104d1d018b121430
mauricio <mauribrunner@gmail.com>
2022-11-23 18:27:56 -0300

    tests fixed

--- bio-api/tests/test_genes-pathways.py
+++ bio-api/tests/test_genes-pathways.py
@@ -5,7 +5,7 @@ URL_BASE = '/genes-pathways'
 
 def test_valid_source_and_externalid(client):
     """Tests valid source an external_id"""
-    source = "HumanCyc"
+    source = "humancyc"
     external_id = "BETA-ALA-DEGRADATION-I-PWY"
     response = client.get(f'{URL_BASE}/{source}/{external_id}')
     res = json.loads(response.data)
@@ -18,18 +18,17 @@ def test_valid_source_and_externalid(client):
 
 def test_invalid_source(client):
     """Tests invalid source"""
-    source = "Ciscate"
+    source = "ciscate"
     external_id = "path:hsa05206"
     response = client.get(f'{URL_BASE}/{source}/{external_id}')
     res = json.loads(response.data)
-    assert response.status_code == 200
-    assert type(res["genes"]) == list
-    assert len(res["genes"]) == 0
+    assert response.status_code == 404
+    assert res["error"] == '404 Not Found: '+source+' is an invalid pathway source'
 
 
 def test_invalid_external_id(client):
     """Tests invalid external_id"""
-    source = "KEGG"
+    source = "kegg"
     external_id = "SrThompson"
     response = client.get(f'{URL_BASE}/{source}/{external_id}')
     res = json.loads(response.data)
