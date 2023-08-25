1da28cec95386698d81cc5773afc3137f1d31489
Julián Muhlberger <julian.muhlberger@gmail.com>
2023-08-25 18:38:57 -0300

    test 2

--- tools/check_existent_tag_in_repo.sh
+++ tools/check_existent_tag_in_repo.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 VERSION="$1"
-/usr/bin/git tag
 TAG=$(git tag -l $VERSION)
-echo $TAG
 if [ -n "$TAG" ]
 then
-  echo "::error::This bio-api version tag already exists in repository."
+  echo "::error::This bio-api version tag ($TAG) already exists in repository."
   exit 1
 fi
